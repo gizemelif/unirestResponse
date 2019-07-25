67633d8f95f1e06a54475c10efad5ca97b2624ef
gizem <gizemelif.atalay@gvg.com.tr>
2019-07-25 10:39:06 +0300

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ import java.util.concurrent.atomic.AtomicReference;
 public class Main {
     public static void main(String[] args) throws IOException{
 
-        String filePath = "C:\\Users\\geatalay\\Desktop\\Vergi_numarasi_kontrol\\data_ankara.json";
+        String filePath = "C:\\Users\\geatalay\\Desktop\\Vergi_numarasi_kontrol\\ankara_tc_no_sorgu.json";
         String fileString = new String(Files.readAllBytes(Paths.get(filePath)),StandardCharsets.UTF_8);
 
 
@@ -34,21 +34,27 @@ public class Main {
         for(int i=0; i<jsonArray.length(); i++) {
 
             JSONObject object = jsonArray.getJSONObject(i);
-            String taxNumber = "";
+            //String taxNumber = "";
+            String gvgUid = "";
             String governmentNumber = "";
             String cityName = "";
 
-            try {
+            /*try {
 
                 taxNumber = valueToStringOrEmpty(object, "TAX_NUMBER");
 
             } catch (Exception e) {
                 e.printStackTrace();
-            }
+            }*/
 
-            /*try{
+            try{
+                gvgUid = valueToStringOrEmpty(object,"GVG_UID");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            try{
                 governmentNumber = valueToStringOrEmpty(object,"GOVERNMENT_NUMBER");
-            }catch (Exception e){e.printStackTrace();}*/
+            }catch (Exception e){e.printStackTrace();}
             try {
                 cityName = object.getString("NAME");
             } catch (Exception e) {
@@ -63,7 +69,7 @@ public class Main {
                     break;
                 }
             }*/
-            String taxApt = new String(Files.readAllBytes(Paths.get("C:\\Users\\geatalay\\Desktop\\Vergi_numarasi_kontrol\\vergiDaireleri")), StandardCharsets.UTF_8);
+            String taxApt = new String(Files.readAllBytes(Paths.get("C:\\Users\\geatalay\\Desktop\\Vergi_numarasi_kontrol\\ankaraVD")), StandardCharsets.UTF_8);
             JSONObject vergiDaireleriKod = new JSONObject(taxApt);
 
             Iterator<?> keys = vergiDaireleriKod.keys();
@@ -75,7 +81,7 @@ public class Main {
                 AtomicReference<Boolean> isFound = new AtomicReference<>(false);
 
                 HttpResponse jsonResponse = Unirest.post("https://ivd.gib.gov.tr/tvd_server/dispatch")
-                        .body("cmd=vergiNoIslemleri_vergiNumarasiSorgulama&callid=1e3cfc764dfbb-11&pageName=R_INTVRG_INTVD_VERGINO_DOGRULAMA&token=d1078f5e3dc646b78d5d4e5842f21e97feb48d366bc7617458b6679dec12675154a01fccc42292bb04d926bc259dbc75e39dd8e202535fd70a7098396c74a6f7&jp=%7B%22dogrulama%22%3A%7B%22vkn1%22%3A%22" + taxNumber + "%22%2C%22tckn1%22%3A%22%22%2C%22iller%22%3A%22034%22%2C%22vergidaireleri%22%3A%22" + key + "%22%7D%2C%22dyntab2%22%3A%7B%22tckn%22%3A%22%22%2C%22vkn%22%3A%22%22%2C%22unvan%22%3A%22%22%2C%22vergidairesi%22%3A%22%22%2C%22faaliyetdurum%22%3A%22%22%7D%2C%22dyntab3%22%3A%7B%22tckn%22%3A%22%22%2C%22vkn%22%3A%22%22%2C%22unvan%22%3A%22%22%2C%22vergidairesi%22%3A%22%22%2C%22faaliyetdurum%22%3A%22%22%7D%2C%22dyntab4%22%3A%7B%22tckn%22%3A%22%22%2C%22vkn%22%3A%22%22%2C%22unvan%22%3A%22%22%2C%22vergidairesi%22%3A%22%22%2C%22faaliyetdurum%22%3A%22%22%7D%2C%22dyntab5%22%3A%7B%22tckn%22%3A%22%22%2C%22vkn%22%3A%22%22%2C%22unvan%22%3A%22%22%2C%22vergidairesi%22%3A%22%22%2C%22faaliyetdurum%22%3A%22%22%7D%7D")
+                        .body("cmd=vergiNoIslemleri_vergiNumarasiSorgulama&callid=1e3cfc764dfbb-11&pageName=R_INTVRG_INTVD_VERGINO_DOGRULAMA&token=d1078f5e3dc646b78d5d4e5842f21e97feb48d366bc7617458b6679dec12675154a01fccc42292bb04d926bc259dbc75e39dd8e202535fd70a7098396c74a6f7&jp=%7B%22dogrulama%22%3A%7B%22vkn1%22%3A%22vkn%22%2C%22tckn1%22%3A%22%22%2C%22iller%22%3A%22034%22%2C%22vergidaireleri%22%3A%22" + key + "%22%7D%2C%22dyntab2%22%3A%7B%22"+governmentNumber+"%22%3A%22%22%2C%22vkn%22%3A%22%22%2C%22unvan%22%3A%22%22%2C%22vergidairesi%22%3A%22%22%2C%22faaliyetdurum%22%3A%22%22%7D%2C%22dyntab3%22%3A%7B%22tckn%22%3A%22%22%2C%22vkn%22%3A%22%22%2C%22unvan%22%3A%22%22%2C%22vergidairesi%22%3A%22%22%2C%22faaliyetdurum%22%3A%22%22%7D%2C%22dyntab4%22%3A%7B%22tckn%22%3A%22%22%2C%22vkn%22%3A%22%22%2C%22unvan%22%3A%22%22%2C%22vergidairesi%22%3A%22%22%2C%22faaliyetdurum%22%3A%22%22%7D%2C%22dyntab5%22%3A%7B%22tckn%22%3A%22%22%2C%22vkn%22%3A%22%22%2C%22unvan%22%3A%22%22%2C%22vergidairesi%22%3A%22%22%2C%22faaliyetdurum%22%3A%22%22%7D%7D")
                         .header("accept", "application/json")
                         .header("accept-language", "en-US,en;q=0.9")
                         .header("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
@@ -96,7 +102,7 @@ public class Main {
                             Data data = new Data();
                             data.setDurum("N/A");
                             data.setDurum_text("N/A");
-                            data.setVkn(taxNumber);
+                            //data.setVkn(taxNumber);
                             data.setTckn(governmentNumber);
                             data.setVdkodu(cityName);
                             vd.setData(data);
@@ -137,7 +143,7 @@ public class Main {
     }
 
     public static void writeFile(String str){
-        File fileName = new File("C:\\Users\\geatalay\\Desktop\\Vergi_numarasi_kontrol\\ankara.txt");
+        File fileName = new File("C:\\Users\\geatalay\\Desktop\\Vergi_numarasi_kontrol\\ankaraTcknSonuc.txt");
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName,true));
             writer.append(str);
